9f9fb81e119d61b0d6d4fdbb2c458e762450fffb
ccha0228 <ccha0228@student.monash.edu>
2024-07-29 14:37:52 +1000

    week1

---
+++ src/components/Myname.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Welcome to My Vue App!</h1>
+        <p>My name is {{ name }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: "Chengqian Chang"
+        };
+    }
+};
+</script>
+
+<style>
+/* Add your custom styles here */
+</style>
